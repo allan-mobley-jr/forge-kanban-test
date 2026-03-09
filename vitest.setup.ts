acd65a71a1729483b6dc5c535c0ddc8fdfbcdacd
Allan Mobley <allan.mobley@mobsites.com>
2026-03-09 15:50:49 -0400

    chore: add ResizeObserver polyfill to test setup

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,1 +1,10 @@
 import '@testing-library/jest-dom/vitest'
+
+// Polyfill ResizeObserver for cmdk and other components that need it
+if (typeof globalThis.ResizeObserver === 'undefined') {
+  globalThis.ResizeObserver = class ResizeObserver {
+    observe() {}
+    unobserve() {}
+    disconnect() {}
+  } as unknown as typeof globalThis.ResizeObserver;
+}
