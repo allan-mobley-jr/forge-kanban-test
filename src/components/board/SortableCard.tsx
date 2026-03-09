b9ab1283308374d7191e216c51c3d62896309718
Allan Mobley <allan.mobley@mobsites.com>
2026-03-09 15:32:18 -0400

    feat: drag-and-drop cards between and within columns (#27)
    * chore: install @dnd-kit/core, @dnd-kit/sortable, @dnd-kit/utilities
    
    * feat: drag-and-drop cards between and within columns (closes #9)

---
+++ src/components/board/SortableCard.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import { useSortable } from "@dnd-kit/sortable";
+import { CSS } from "@dnd-kit/utilities";
+import type { Card as CardType } from "@/lib/types";
+import { Card } from "./Card";
+
+interface SortableCardProps {
+  card: CardType;
+  onClick?: () => void;
+  isDragOverlay?: boolean;
+}
+
+export function SortableCard({ card, onClick, isDragOverlay }: SortableCardProps) {
+  const {
+    attributes,
+    listeners,
+    setNodeRef,
+    transform,
+    transition,
+    isDragging,
+  } = useSortable({ id: card.id });
+
+  const style = {
+    transform: CSS.Transform.toString(transform),
+    transition,
+  };
+
+  return (
+    <div
+      ref={setNodeRef}
+      style={style}
+      {...attributes}
+      {...listeners}
+      className={isDragging || isDragOverlay ? "opacity-40" : ""}
+    >
+      <Card card={card} onClick={!isDragging ? onClick : undefined} />
+    </div>
+  );
+}
